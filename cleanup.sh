30a3b4192408b6b96d6028617872d2563f7a7be8
Jan Šilhan <silhanu@gmail.com>
2018-03-31 20:07:36 +0200

    Updated cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+# Stop all images
+docker stop $(docker ps -a -q)
 # Delete all containers
-docker rm $(docker ps -a -q)
+docker rm --force $(docker ps -a -q)
 # Delete all images
-docker rmi $(docker images -q)
+docker rmi --force $(docker images -q)
