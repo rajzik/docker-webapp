da75cc41cb26165bc131041a35f8fac48b5827de
root <root@localhost.localdomain>
2018-04-01 15:05:11 +0200

    Added messages

--- server/users/models.py
+++ server/users/models.py
@@ -1,17 +1,44 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(models.Model):
-    id = models.AutoField(primary_key= True)
+    id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=62)
     is_superuser = models.BooleanField()
-    date_joined = models.TimeField(auto_now= True)
+    date_joined = models.TimeField(auto_now=True)
     last_login = models.TimeField()
     friends = models.ManyToManyField('self')
+    messages = models.ManyToManyField(
+        'Message',
+        through='UserMessages',
+        through_fields=('user1', 'message')
+    )
 
     class Meta:
         db_table = 'user'
+
+
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    from_user = models.ForeignKey(User, related_name= 'from_user_id', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name= 'to_user_id', on_delete=models.CASCADE)
+    time = models.TimeField(auto_now=True)
+    message = models.CharField(max_length=550)
+    
+    class Meta:
+        db_table = 'message'
+
+
+class UserMessages(models.Model):
+    id = models.AutoField(primary_key=True)
+    user1 = models.ForeignKey(
+        User, related_name='user1_id', on_delete=models.CASCADE)
+    user2 = models.ForeignKey(
+        User, related_name='user2_id', on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'user_messages'
--- server/webapp/schema.py
+++ server/webapp/schema.py
@@ -1,11 +1,16 @@
 from graphene_django import DjangoObjectType
 import graphene
-from users.models import User
+from users.models import User, Message
 
 
 class UserType(DjangoObjectType):
     class Meta:
         model = User
+
+class MessageType(DjangoObjectType):
+
+    class Meta:
+        model = Message
 
 class Query(graphene.ObjectType):
     users = graphene.List(UserType, description='A few billion people')
@@ -14,7 +19,17 @@ class Query(graphene.ObjectType):
         id=graphene.ID(),
         description='Just one person belonging to an ID',
     )
+    message = graphene.Field(
+        MessageType,
+        id=graphene.ID(),
+        description='Just one message belonging to an ID',
+    )
+    messages = graphene.List(MessageType, description='Messages')
 
+    def resolve_messages(self, info):
+        return Message.objects.all()
+    def resolve_message(self, info, id):
+        return Message.objects.get(pk=id)
     def resolve_users(self, info):
         return User.objects.all()
     def resolve_user(self, info, id):
