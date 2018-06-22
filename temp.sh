efd53136853bde525dd5b8aa844fc5472c08e205
Jan Šilhan <silhanu@gmail.com>
2018-06-22 18:19:11 +0200

    'wtf'

--- temp.sh
+++
@@ -1,6 +0,0 @@
-export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://192.168.99.100:2376"
-export DOCKER_CERT_PATH="/home/rajzik/.docker/machine/machines/myvm1"
-export DOCKER_MACHINE_NAME="myvm1"
-# Run this command to configure your shell: 
-# eval $(docker-machine env myvm1)
