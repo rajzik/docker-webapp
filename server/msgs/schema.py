ebdd1b6ca42163b6aca94c22cf307404fc504bf8
jan.silhan <jan.silhan@ixperta.com>
2018-06-27 13:08:13 +0200

    Backend is done

--- server/msgs/schema.py
+++ server/msgs/schema.py
@@ -7,15 +7,44 @@ from graphene_django import DjangoObjectType
 from graphql import GraphQLError
 
 
-from .models import Message
+from .models import Message, UserMessages
+
+from users.models import User
 
 class MessageType(DjangoObjectType):
     class Meta:
         model = Message
 
+class SendMessage(graphene.Mutation):
+    message = graphene.String()
+
+    class Arguments:
+        message = graphene.String()
+        user_id = graphene.Int()
+    
+    def mutate(self, info, message, user_id):
+        user = info.context.user
+
+        if user.is_anonymous:
+            raise GraphQLError('You must be logged to send message!')
+        
+        toUser = User.objects.filter(id=user_id).first()
+
+        if not toUser:
+            raise Exception('Invalid user!')
+
+        msg = Message(from_user=user, message=message)
+        msg.save()
+
+        UserMessages(user1=user, user2=toUser, message=msg).save()
+        UserMessages(user2=user, user1=toUser, message=msg).save()
+        
+        return SendMessage(message)
+
+
+
 class Mutation(graphene.ObjectType):
-    # TODO: Add more stuff
-    pass
+    send_message = SendMessage.Field()
 
 class Query(graphene.ObjectType):
     messages = graphene.List(
